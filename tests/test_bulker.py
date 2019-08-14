9ed109cb5016d92bc4004d15c3e5a54f69a79c77
nsheff <nsheff@users.noreply.github.com>
2019-08-14 08:35:41 -0400

    no local test

--- tests/test_bulker.py
+++ tests/test_bulker.py
@@ -3,26 +3,30 @@ import subprocess
 import yacman
 from bulker.bulker import DEFAULT_CONFIG_FILEPATH
 
-def capture(command):
-    proc = subprocess.Popen(command,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE,
-    )
-    out,err = proc.communicate()
-    return out, err, proc.returncode
-
 
 def test_yacman():
 
-	bc = yacman.YacAttMap(filepath=DEFAULT_CONFIG_FILEPATH)
-	bc
-	bc.bulker.default_crate_folder
+    bc = yacman.YacAttMap(filepath=DEFAULT_CONFIG_FILEPATH)
+    bc
+    bc.bulker.default_crate_folder
 
-	out, err, exitcode = capture([os.path.expandvars("$HOME/.local/bin/bulker"), "load", "demo"])
-	assert exitcode == 0
-	out, err, exitcode = capture([os.path.expandvars("$HOME/.local/bin/bulker"), "load", "bogusbogus"])
-	assert exitcode == 1
+    # Not straightforward to test the CLI directly with pytest: This works on
+    # local computer but not on travis; it can't find the executable
 
-	# manifest = yacman.YacAttMap(filepath="/home/ns5bc/code/bulker/demo/demo_manifest.yaml")
-	# bc
-	 
+    # def capture(command):
+    # proc = subprocess.Popen(command,
+    #     stdout = subprocess.PIPE,
+    #     stderr = subprocess.PIPE,
+    # )
+    # out,err = proc.communicate()
+    # return out, err, proc.returncode
+
+
+    # out, err, exitcode = capture([os.path.expandvars("$HOME/.local/bin/bulker"), "load", "demo"])
+    # assert exitcode == 0
+    # out, err, exitcode = capture([os.path.expandvars("$HOME/.local/bin/bulker"), "load", "bogusbogus"])
+    # assert exitcode == 1
+
+    # manifest = yacman.YacAttMap(filepath="/home/ns5bc/code/bulker/demo/demo_manifest.yaml")
+    # bc
+     
