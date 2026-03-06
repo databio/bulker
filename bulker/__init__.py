3b95cc59d7abe64bf9b43e2ba980eb779fccf2f4
nsheff <nsheff@users.noreply.github.com>
2026-03-05 21:37:13 -0500

    Remove logmuse.init_logger from __init__.py
    Calling init_logger at import time prevents the logger from
    inheriting configuration from a root logger, breaking logging
    when bulker is used as a library.

--- bulker/__init__.py
+++ bulker/__init__.py
@@ -1,9 +1,4 @@
-# Project configuration, particularly for logging.
-
-import logmuse
 from ._version import __version__
 
 __classes__ = []
 __all__ = __classes__ + []
-
-logmuse.init_logger("bulker")
